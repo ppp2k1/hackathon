49091b098857a4f723ee520e11394f3ff55ffeb7
ppp2k1 <mailparaghere@gmail.com>
2020-02-04 16:05:41 +0530

    added configuration service

--- configuration-service/src/main/java/com/netapp/configuration/ConfigurationService.java
+++ configuration-service/src/main/java/com/netapp/configuration/ConfigurationService.java
@@ -1,4 +1,12 @@
 package com.netapp.configuration;
 
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
 public class ConfigurationService {
+    public static void main(String[] args) {
+        run(ConfigurationService.class, args);
+    }
 }
--- graphql-gateway/src/main/java/com/netapp/monitoring/GraphQLGateway.java
+++ graphql-gateway/src/main/java/com/netapp/monitoring/GraphQLGateway.java
@@ -1,10 +1,14 @@
 package com.netapp.monitoring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class MonitoringServiceApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonitoringServiceApplication.class, args);
