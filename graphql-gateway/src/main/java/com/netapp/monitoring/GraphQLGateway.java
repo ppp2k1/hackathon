b6bcbcaad47882581add96fa4cdfaba66826c876
ppp2k1 <mailparaghere@gmail.com>
2020-02-04 16:52:58 +0530

    changes to docker build

--- graphql-gateway/src/main/java/com/netapp/monitoring/GraphQLGateway.java
+++ graphql-gateway/src/main/java/com/netapp/monitoring/GraphQLGateway.java
@@ -1,17 +1,15 @@
 package com.netapp.monitoring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class MonitoringServiceApplication {
+public class GraphQLGateway {
 
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(MonitoringServiceApplication.class, args);
+		SpringApplication.run(GraphQLGateway.class, args);
 	}
 
 }
