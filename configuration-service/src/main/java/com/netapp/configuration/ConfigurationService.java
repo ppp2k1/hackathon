d793b8c5b92fb5b58ed293080cc9f48580ef09c2
ppp2k1 <mailparaghere@gmail.com>
2020-02-05 16:35:13 +0530

    updating Configuration Service

--- configuration-service/src/main/java/com/netapp/configuration/ConfigurationService.java
+++ configuration-service/src/main/java/com/netapp/configuration/ConfigurationService.java
@@ -1,10 +1,18 @@
 package com.netapp.configuration;
 
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
 import static org.springframework.boot.SpringApplication.*;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableConfigServer
 public class ConfigurationService {
     public static void main(String[] args) {
         run(ConfigurationService.class, args);
