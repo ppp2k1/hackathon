22de34858ff473c1646ff5565eaaf0ed00eeaa93
Vuppalapati <umav@netapp.com>
2020-02-05 16:41:50 +0530

    Made vmware-service to run on docker and register to service-registry

--- vmware-service/src/main/java/com/netapp/security/vm/VmApplication.java
+++ vmware-service/src/main/java/com/netapp/security/vm/VmApplication.java
@@ -2,8 +2,10 @@ package com.netapp.security.vm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VmApplication {
 
 	public static void main(String[] args) {
